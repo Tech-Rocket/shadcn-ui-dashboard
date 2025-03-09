0861fbb1e2c0a6eeb58891b7d2d9d87e957c598a
Ebee02 <beelawrence8@gmail.com>
2025-03-09 14:16:50 +0000

    Added the Sidebar to the RootLayout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,42 @@
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import Link from "next/link";
+import {
+  LayoutDashboard,
+  Newspaper,
+  Folders,
+  CreditCard,
+  Settings,
+  Users,
+} from "lucide-react";
+
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <Command className="bg-secondary border border-gray-300 shadow-md rounded-none">
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem>Calendar</CommandItem>
+          <CommandItem>Search Emoji</CommandItem>
+          <CommandItem>Calculator</CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>Profile</CommandItem>
+          <CommandItem>Billing</CommandItem>
+          <CommandItem>Settings</CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
 }

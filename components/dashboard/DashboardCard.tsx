e19fe7dd987d2ba917b49f8f68a53a017f9f0eef
Ebee02 <beelawrence8@gmail.com>
2025-03-17 09:28:11 +0000

    update

--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -1,16 +1,21 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
+import { Newspaper } from "lucide-react";
 
 export default function DashboardCard() {
   return (
     <>
-      <h1>Dashboard Card</h1>
+      <Card className="bg-slate-100 dark:bg-slate-800 p-5 rounded-sm">
+        <CardContent>
+          <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">
+            Posts
+          </h3>
+
+          <div className="text-slate-500 flex gap-5 justify-center items-center">
+            <Newspaper size={59} />
+            <h3 className="text-5xl font-semibold dark:text-slate-200">100</h3>
+          </div>
+        </CardContent>
+      </Card>
     </>
   );
 }

ba6d76d2bfbd8363f31f1615ab647eae81f0a02b
Ebee02 <beelawrence8@gmail.com>
2025-03-27 16:13:39 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         />
       </div>
 
-      <PostTable />
+      <PostTable title="Latest Posts" />
     </>
   );
 }
--- components/posts/PostTables.tsx
+++ components/posts/PostTables.tsx
@@ -1,3 +1,41 @@
-export default function PostTable() {
-  return <div>post table</div>;
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCaption,
+} from "@/components/ui/table";
+
+import Link from "next/link";
+import posts from "@/data/post";
+import { PostType } from "@/types/postType";
+
+interface PostTableProps {
+  limit?: number;
+  title?: string;
+}
+
+export default function PostTable(props: PostTableProps) {
+  return (
+    <div className="mt-8">
+      <h3 className="text-2xl mb-4 font-semibold">
+        {props.title ? props.title : "Posts"}
+      </h3>
+
+      <Table>
+        {/* <TableCaption>A list of recent post</TableCaption> */}
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead className="hidden md:table-cell">Author</TableHead>
+            <TableHead className="hidden md:table-cell text-right">
+              Date
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+      </Table>
+    </div>
+  );
 }
